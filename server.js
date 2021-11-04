324f4b05ecb830bb4879076334a78a98606f8723
Relton Fidelis ME <contato.reltonfidelis@gmail.com>
2021-11-04 09:38:04 -0300

    Avançando no Curso : Bootcamo Eduzz Fullstack Developer : DIO

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostUrl = 'http://localhost';
+const port = 3000;
+const stats = require('./pcRamUsase');
+http.createServer((req, res) => {
+    const url = req.url;
+    if(url === '/stats'){
+    res.end(JSON.stringify(stats, null, 2));
+    }else{
+        res.end('<h1> Seja bem vindo</h1>')
+    }
+}).listen(3000, () => console.log(`Server running in ${hostUrl}:${port}, to open on web browser. ${stats}`))
